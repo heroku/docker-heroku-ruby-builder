d510a6dfceea677eb246d6b3e53ecbd6580d1488
Terence Lee <hone02@gmail.com>
2014-08-07 17:49:01 -0700

    use cedar-14 for building

--- rubies/ruby-2.0.0.sh
+++ rubies/ruby-2.0.0.sh
@@ -2,4 +2,4 @@
 
 source `dirname $0`/common.sh
 
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.0.0-p481 hone/ruby-builder:cedar
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.0.0-p481 hone/ruby-builder:cedar-14
--- rubies/ruby-2.1.2.sh
+++ rubies/ruby-2.1.2.sh
@@ -2,4 +2,4 @@
 
 source `dirname $0`/common.sh
 
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.1.2 hone/ruby-builder:cedar
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.1.2 hone/ruby-builder:cedar-14
