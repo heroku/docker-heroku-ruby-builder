b031f32b61a3269b918884648bef7ffac595dcb1
Terence Lee <hone02@gmail.com>
2015-02-27 17:28:04 -0600

    ruby 1.8.7-p376

---
+++ rubies/ruby-1.8.7-p376.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source `dirname $0`/common.sh
+
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION="1.8.7-p376@44719" -e SVN_URL="http://svn.ruby-lang.org/repos/ruby/trunk" -e RELNAME="branches/ruby_1_8_7@44719" -e RUBYGEMS_VERSION=1.8.24 hone/ruby-builder:cedar-14
+
