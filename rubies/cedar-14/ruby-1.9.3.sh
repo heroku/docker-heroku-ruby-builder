a98693134508793246888297bc7015e7b96419d0
Terence Lee <hone02@gmail.com>
2015-04-29 18:14:07 -0500

    remove nonpatchlevel ruby versions

--- rubies/cedar-14/ruby-1.9.3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source `dirname $0`/../common.sh
-
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=1.9.3-p545 -e STACK=cedar-14 hone/ruby-builder:cedar-14
--- rubies/cedar-14/ruby-build-1.9.2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source `dirname $0`/../common.sh
-
-docker run -v `pwd`/cache:/tmp/cache -v `pwd`/builds:/tmp/output -e VERSION=1.9.2-p328 -e SVN_URL="http://svn.ruby-lang.org/repos/ruby/trunk" -e RELNAME="branches/ruby_1_9_2" -e BUILD:true -e STACK=cedar-14 hone/ruby-builder:cedar-14
