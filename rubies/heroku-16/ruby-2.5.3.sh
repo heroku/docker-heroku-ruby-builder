1c8df800c3734e33bec2d6b19b090e680bd25d5e
schneems <richard.schneeman+foo@gmail.com>
2019-03-05 12:01:48 -0600

    Patch RubyGems vulnerabilities

--- rubies/heroku-16/ruby-2.5.3.sh
+++ rubies/heroku-16/ruby-2.5.3.sh
@@ -2,4 +2,9 @@
 
 source `dirname $0`/../common.sh
 
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.5.3  -e STACK=heroku-16 hone/ruby-builder:heroku-16
+docker run -v $OUTPUT_DIR:/tmp/output \
+           -v $CACHE_DIR:/tmp/cache \
+           -e VERSION=2.5.3 \
+           -e STACK=heroku-16 \
+           -e PATCH_URL=https://gist.githubusercontent.com/schneems/fd2bd841515367871e5b332afe9455ea/raw/1de62a84bce330c9ca8336fddb7c143c36a01a1d/ruby-2.5.3-rubygems.patch \
+           hone/ruby-builder:heroku-16
---
+++ rubies/heroku-16/ruby-2.6.1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source `dirname $0`/../common.sh
+
+docker run -v $OUTPUT_DIR:/tmp/output \
+-v $CACHE_DIR:/tmp/cache \
+-e VERSION=2.6.1 \
+-e PATCH_URL=https://gist.githubusercontent.com/schneems/fd2bd841515367871e5b332afe9455ea/raw/1de62a84bce330c9ca8336fddb7c143c36a01a1d/ruby-2.6.1-rubygems.patch \
+-e STACK=heroku-16 \
+hone/ruby-builder:heroku-16
