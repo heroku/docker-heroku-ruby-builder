9440985514e20abb3ca6aaba5ffaac86820b8106
schneems <richard.schneeman+foo@gmail.com>
2019-03-08 09:25:34 -0600

    Patch Ruby 2.5.x
    Includes CVE fixes that were just released in addition to bundler 2 fixes from https://bugs.ruby-lang.org/issues/15643
    
    ```
    bash rubies/heroku-18/ruby-2.5.3.sh
    bash rubies/heroku-16/ruby-2.5.3.sh
    bash rubies/cedar-14/ruby-2.5.3.sh
    
    bash rubies/heroku-18/ruby-2.5.2.sh
    bash rubies/heroku-16/ruby-2.5.2.sh
    bash rubies/cedar-14/ruby-2.5.2.sh
    
    
    bash rubies/heroku-18/ruby-2.5.1.sh
    bash rubies/heroku-16/ruby-2.5.1.sh
    bash rubies/cedar-14/ruby-2.5.1.sh
    
    
    bash rubies/heroku-18/ruby-2.5.0.sh
    bash rubies/heroku-16/ruby-2.5.0.sh
    bash rubies/cedar-14/ruby-2.5.0.sh
    ```
    
    Ruby 2.5.0 was already previously patched, so that patch was combined with the other two.

--- rubies/cedar-14/ruby-2.5.0-rubygems-276-for-ruby25.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source `dirname $0`/../common.sh
-
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.5.0 -e PATCH_URL=https://bugs.ruby-lang.org/attachments/download/7027/rubygems-276-for-ruby25.patch -e STACK=cedar-14 hone/ruby-builder:cedar-14
--- rubies/cedar-14/ruby-2.5.0.sh
+++ rubies/cedar-14/ruby-2.5.0.sh
@@ -2,4 +2,9 @@
 
 source `dirname $0`/../common.sh
 
-docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=2.5.0  -e STACK=cedar-14 hone/ruby-builder:cedar-14
+docker run -v $OUTPUT_DIR:/tmp/output \
+           -v $CACHE_DIR:/tmp/cache \
+           -e VERSION=2.5.0 \
+           -e STACK=cedar-14 \
+           -e PATCH_URL=https://gist.githubusercontent.com/schneems/8c1c2eae9b255e9e1c18d95bffbb9d9f/raw/31b830252f277646a14ddc73d8e0601a847615f1/rubygems-276-for-ruby25-and-rubygems-cve-and-bundler2 \
+           hone/ruby-builder:cedar-14
