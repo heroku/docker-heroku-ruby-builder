f9e68f268e779ef9a60827bded5122ce2e6cdafd
Richard Schneeman <rschneeman@salesforce.com>
2026-06-19 21:01:29 -0500

    Add jruby_changelog test coverage parity
    Extract a pure render_jruby_changelog helper so the changelog output
    format can be unit tested without a network fetch, and add a
    regular_release test mirroring ruby_changelog. Adds pretty_assertions
    as a dev-dependency.

--- jruby_executable/src/bin/jruby_changelog.rs
+++ jruby_executable/src/bin/jruby_changelog.rs
@@ -11,7 +11,7 @@ struct Args {
     version: JRubyVersion,
 }
 
-fn jruby_changelog<W>(args: &Args, mut io: W) -> Result<W, Box<dyn Error>>
+fn jruby_changelog<W>(args: &Args, io: W) -> Result<W, Box<dyn Error>>
 where
     W: Write,
 {
@@ -19,6 +19,17 @@ where
 
     let stdlib_version = jruby_build_properties(version)?.ruby_stdlib_version()?;
 
+    render_jruby_changelog(version, &stdlib_version, io)
+}
+
+fn render_jruby_changelog<W>(
+    version: &str,
+    stdlib_version: &str,
+    mut io: W,
+) -> Result<W, Box<dyn Error>>
+where
+    W: Write,
+{
     let changelog = formatdoc! {"
         ## JRuby version {version} is now available
 
@@ -47,5 +58,32 @@ fn main() {
         "});
 
         std::process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn regular_release() {
+        let mut io = Vec::new();
+
+        let output = render_jruby_changelog("9.4.7.0", "3.1.4", &mut io).unwrap();
+        let actual = String::from_utf8_lossy(output);
+        let expected = formatdoc! {"
+                ## JRuby version 9.4.7.0 is now available
+
+                [JRuby v9.4.7.0](/articles/ruby-support-reference#supported-jruby-versions) is now available on Heroku. To run
+                your app using this version of Ruby, add the following `ruby` directive to your Gemfile:
+
+                ```ruby
+                ruby \"3.1.4\", engine: \"jruby\", engine_version: \"9.4.7.0\"
+                ```
+
+                The JRuby release notes can be found on the [JRuby website](https://www.jruby.org/news).
+            "};
+        assert_eq!(expected.trim(), actual.trim());
     }
 }
